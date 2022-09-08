eec5e4649fb18c2476d1e2a3c013ff9b8e2d2913
kinderoth <kinderoth@users.noreply.github.com>
2022-09-08 16:20:30 -0400

    Add files via upload

---
+++ students.h
@@ -0,0 +1,14 @@
+#include iostream
+using namespace std;
+
+class Student { 
+    public:
+        int ID;
+        char name_First;
+        char name_Last;
+        char email;
+        int age;
+        char degree;
+
+
+}
