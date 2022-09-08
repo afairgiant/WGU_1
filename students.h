2e8af2bb03ec5c4764eb605cdd367fe9373059b3
kinderoth <kinderoth@users.noreply.github.com>
2022-09-08 18:38:57 -0400

    Finished Setup of Students.h

--- students.h
+++ students.h
@@ -11,15 +11,30 @@ class Student {
 public:
     Student();
 
-    // DECLARE ACCESSORS - Section D2.A
+    // DECLARE ACCESSORS - Section D2.a
+    string getStudentID() const;
+    string getFirstName() const;
+    string getLastName() const;
+    string getEmailAddress() const;
+    int getAge() const;
+    int* getDaysInCourse() const;
+    degreeProgram getDegree() const;
 
-    // DECCLARE MUTATORS - Section D2.B
+    // DECCLARE MUTATORS - Section D2.b
+    void setStudentID(string studentID);
+    void setFirstName(string firstName);
+    void setLastName(string lastName);
+    void setEmailAddress(string emailAddress);
+    void setAge(int age);
+    void setDaysInCourse(int daysInCourse[]);
+    void setDegree(degreeProgram degree);
 
-    // CONSTRUCTOR - Section D2.D
+    // CONSTRUCTOR - Section D2.d
     Student(string studentID, string nameFirst, string nameLast, string email, int age, int daysInCourse[], degreeProgram degree);
+
 private:
 
-    // Data variables - Section D1
+    // Data Members - Section D1
     string studentID;
     string firstName;
     string lastName;
